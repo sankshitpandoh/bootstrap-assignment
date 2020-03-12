7605c8126d62e0bf7ee4e3ff2de08c98a47b1a4b
Sankshit Pandoh <sankshit.p@uplers.in>
2020-03-12 16:10:47 +0530

    our client section added

--- assets/js/general.js
+++ assets/js/general.js
@@ -8,4 +8,13 @@ owl.owlCarousel({
     autoplayTimeout:6000,
     autoplayHoverPause:true
 });
+var owl = $('.c-slider');
+owl.owlCarousel({
+  items:6,
+  loop:true,
+  margin:30,
+  autoplay:true,
+  autoplayTimeout:6000,
+  autoplayHoverPause:true
+});
 });
