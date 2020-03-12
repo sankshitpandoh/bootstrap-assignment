f41e4a3ffb5b9a1a7c283c215c6428220c2ec2ea
Sankshit Pandoh <sankshit.p@uplers.in>
2020-03-12 13:54:59 +0530

    slider added

---
+++ assets/js/general.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var owl = $('.t-slider');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplayHoverPause:true
+});
+});
