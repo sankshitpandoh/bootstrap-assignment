d963ef269de7d133daaa5a1b4a073ab66880a557
Sankshit Pandoh <sankshit.p@uplers.in>
2020-03-16 12:22:59 +0530

    animations added

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
+
+  new WOW().init();
+
+
+
   var owl = $('.t-slider');
-owl.owlCarousel({
+  owl.owlCarousel({
     items:1,
     loop:true,
     margin:10,
@@ -8,13 +13,18 @@ owl.owlCarousel({
     autoplayTimeout:6000,
     autoplayHoverPause:true
 });
-var owl = $('.c-slider');
-owl.owlCarousel({
+ var owl = $('.c-slider');
+ owl.owlCarousel({
   items:6,
   loop:true,
   margin:30,
   autoplay:true,
   autoplayTimeout:5000,
   autoplayHoverPause:true
+});
+
+$('.counter').counterUp({
+delay: 10,
+time: 2000
 });
 });
