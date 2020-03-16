125425c818e983c76939318faf46d0b124426c60
Sankshit Pandoh <sankshit.p@uplers.in>
2020-03-16 16:20:14 +0530

    isotope js setted up for portfolio section // working but designing pending

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,9 +1,63 @@
 $(document).ready(function(){
 
+  // header-tab to make header solid on scroll
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.header-main').addClass('header-scrolled');
+      $('.top-bar').addClass('bar-scrolled');
+    } else {
+      $('.header-main').removeClass('header-scrolled');
+      $('.top-bar').removeClass('bar-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('.header-main').addClass('header-scrolled');
+    $('.top-bar').addClass('bar-scrolled');
+  }
+
+  // wow js to add scroll bounce effect
   new WOW().init();
 
+// counter tab - to make numbers toggle
+  $('.counter').counterUp({
+  delay: 10,
+  time: 2000
+  });
+
+// our portfolio - isotope js to display selecetd content
+  // $('.port-container').isotope({
+  //   itemSelector: '.port-item',
+  //   layoutMode: 'fitRows'
+  // });
+  //
+  // $('.port-nav li').click(function(){
+  //   $('.port-nav li').removeClass('current-active');
+  //   $(this).addClass('current-active');
+  //
+  //   var selector = $(this).data('filter');
+  //   $('port-container').isotope({
+  //     filter: selector
+  //   });
+  //   return false
+  // });
+
+  // $(window).on('load', function() {
+  var portfolioIsotope = $('.port-container').isotope({
+    itemSelector: '.port-item'
+  });
+  $('.port-nav li').on('click', function() {
+    $(".port-nav li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({
+      filter: $(this).data('filter')
+    });
+  });
+// });
 
 
+// testimonial slider
   var owl = $('.t-slider');
   owl.owlCarousel({
     items:1,
@@ -13,6 +67,7 @@ $(document).ready(function(){
     autoplayTimeout:6000,
     autoplayHoverPause:true
 });
+// client slider
  var owl = $('.c-slider');
  owl.owlCarousel({
   items:6,
@@ -23,8 +78,6 @@ $(document).ready(function(){
   autoplayHoverPause:true
 });
 
-$('.counter').counterUp({
-delay: 10,
-time: 2000
-});
+
+
 });
